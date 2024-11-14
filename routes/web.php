91dae47c29183e03bd8efb4617ba5f582511441b
Akissohje esther <estherakissohe176@gmail.com>
2024-11-14 12:16:03 +0100

    oki

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ContentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/content', [ContentController::class, 'index']);
