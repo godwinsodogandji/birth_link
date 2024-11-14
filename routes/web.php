bb80cce2b6a3eaaff17d0f96b1ef8527a860e9de
arnaudsonkpianphp <arnaudsonkpian205@gmail.com>
2024-11-14 19:09:08 +0100

    OK

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,26 @@
 
 use App\Http\Controllers\AjoutDesAmisController;
 use App\Http\Controllers\AmisSuggererController;
+use App\Http\Controllers\DashbordController;
+use App\Http\Controllers\FriendController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/ajoutdesamis', [AjoutDesAmisController::class, 'index'])->name('ajoutdesamis');
+Route::get('/dashboard', [DashbordController::class, 'index'])->name('dashbord');
+Route::get('/suggestion-des-amis', [AmisSuggererController::class, 'index'])->name('suggestion-des-amis');
+Route::get('/friends', [FriendController::class, 'index'])->name('friends');
+<?php
+
+use App\Http\Controllers\AjoutDesAmisController;
+use App\Http\Controllers\AmisSuggererController;
 use App\Http\Controllers\AnniversaireAvenirController;
 use App\Http\Controllers\AnniversairePasseController;
 use App\Http\Controllers\DashbordController;
 use App\Http\Controllers\NotificationController;
-use App\Http\Controllers\FriendController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -20,4 +35,3 @@ Route::get('/anniversaire-avenir', [AnniversaireAvenirController::class, 'index'
 Route::get('/anniversaire-passe', [AnniversairePasseController::class, 'index'])->name('anniversaire-passe');
 
 Route::get('/suggestion-des-amis', [AmisSuggererController::class, 'index'])->name('suggestion-des-amis');
-Route::get('/friends', [FriendController::class, 'index'])->name('friends');
