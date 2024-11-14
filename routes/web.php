6ab71e9ab9a182e6321d277fa5c212e6aac6d80b
arnaudsonkpianphp <arnaudsonkpian205@gmail.com>
2024-11-14 17:19:37 +0100

    recuperation du code de Chirac

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\AjoutDesAmisController;
 use App\Http\Controllers\AmisSuggererController;
 use App\Http\Controllers\DashbordController;
-use App\Http\Controllers\NotificationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,6 +10,5 @@ Route::get('/', function () {
 });
 
 Route::get('/ajoutdesamis', [AjoutDesAmisController::class, 'index'])->name('ajoutdesamis');
-Route::get('/notification', [NotificationController::class, 'index'])->name('notification');
 Route::get('/dashboard', [DashbordController::class, 'index'])->name('dashbord');
 Route::get('/suggestion-des-amis', [AmisSuggererController::class, 'index'])->name('suggestion-des-amis');
