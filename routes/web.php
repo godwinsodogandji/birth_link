9038c692712801e584af2a6e04ba990b9bbd434b
Akissohje esther <estherakissohe176@gmail.com>
2024-11-15 11:24:37 +0100

    okroute

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AjoutDesAmisController;
 use App\Http\Controllers\AmisSuggererController;
 use App\Http\Controllers\DashbordController;
 use App\Http\Controllers\FriendController;
+use App\Http\Controllers\ThemepersonController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,3 +16,4 @@ Route::get('/dashboard', [DashbordController::class, 'index'])->name('dashbord')
 Route::get('/suggestion-des-amis', [AmisSuggererController::class, 'index'])->name('suggestion-des-amis');
 Route::get('/confirmation-des-amis', [AmisSuggererController::class, 'confirmation'])->name('confirmation-des-amis');
 Route::get('/friends', [FriendController::class, 'index'])->name('friends');
+Route::get('/theme', [ThemepersonController::class, 'index'])->name('theme');
