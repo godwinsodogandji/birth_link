2bced52ab47fc54c4b437e43bd8f69d812a67a03
Christiane Sossoukpe <christdebo68@gmail.com>
2024-11-15 12:09:17 +0100

    Pages cartes populaires ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,78 +18,127 @@
 <body class="flex items-center justify-center min-h-screen p-6 bg-red-100">
   <!-- Sidebar for Buttons -->
   <div class="space-y-6 mr-20">
-    <button onclick="showCard(1)" class="bg-blue-500 text-white py-2 px-4 rounded w-150 hover:bg-blue-600 transition block">Carte de voeux n°1</button>
-    <button onclick="showCard(2)" class="bg-blue-500 text-white py-2 px-4 rounded w-150 hover:bg-blue-600 transition block">Carte de voeux n°2</button>
-    <button onclick="showCard(3)" class="bg-blue-500 text-white py-2 px-4 rounded w-150 hover:bg-blue-600 transition block">Carte de voeux n°3</button>
-    <button onclick="showCard(4)" class="bg-blue-500 text-white py-2 px-4 rounded w-150 hover:bg-blue-600 transition block">Carte de voeux n°4</button>
-    <button onclick="showCard(5)" class="bg-blue-500 text-white py-2 px-4 rounded w-150 hover:bg-blue-600 transition block">Carte de voeux n°5</button>
-    <button onclick="showCard(6)" class="bg-blue-500 text-white py-2 px-4 rounded w-150 hover:bg-blue-600 transition block">Carte de voeux n°6</button>
+    <button onclick="showCard(1)"  style="background-color: rgb(220,20,60);" class="text-white py-2 px-4 rounded w-150  transition block">Carte de voeux n°1</button>
+    <button onclick="showCard(2)" style="background-color: rgb(220,20,60);" class="text-white py-2 px-4 rounded w-150  transition block">Carte de voeux n°2</button>
+    <button onclick="showCard(3)" style="background-color: rgb(220,20,60);" class="text-white py-2 px-4 rounded w-150  transition block">Carte de voeux n°3</button>
+    <button onclick="showCard(4)" style="background-color: rgb(220,20,60);" class="text-white py-2 px-4 rounded w-150  transition block">Carte de voeux n°4</button>
+    <button onclick="showCard(5)" style="background-color: rgb(220,20,60);" class="text-white py-2 px-4 rounded w-150 transition block">Carte de voeux n°5</button>
+    <button onclick="showCard(6)" style="background-color: rgb(220,20,60);" class="text-white py-2 px-4 rounded w-150  transition block">Carte de voeux n°6</button>
   </div>
 </body>
 
 
 
-    <!-- Container for Cards -->
-  <!-- Container for Cards -->
 <!-- Container for Cards -->
 <section class="grid grid-cols-1 gap-4 ml-8">
   <!-- Card 1 -->
-  <div id="card-1" class="card hidden bg-blue-100 p-6 rounded-lg shadow-md text-center
+  <div id="card-1" class="card hidden  p-6 mt-25 rounded-lg shadow-md text-center
        flex items-center justify-center flex-col bg-cover bg-no-repeat bg-center relative" 
-       style="background-image: url('/images/carte1.jpg'); width: 1000px; max-width: 100%; height: 900px;">
-    <p class="text-white font-semibold text-xl">Joyeux anniversaire !</p>
-    <p class="text-white font-semibold text-xl">Que cette année t'apporte bonheur et succès.</p>
-    <button class="absolute bottom-4 right-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition">Envoyer</button>
+       style="background-image: url('/images/carte1.jpg'); width: 700px; max-width: 100%; height: 500px;">
+       <div   style="background-color:#FEBFD2;" class="shadow-lg p-6 rounded-full mt-20">
+    <p class="text-black font-semibold text-xl">Joyeux anniversaire !</p>
+    <p class="text-black font-semibold text-xl">Que cette année t'apporte</p>
+    <p class="text-black font-semibold text-xl"> bonheur et succès.</p>
+</div>
+    <button style="background-color: #e0115f;" class="absolute bottom-4 right-4 text-white py-2 px-4 rounded hover:bg-blue-600 transition">Envoyer</button>
   </div>
 
   <!-- Card 2 -->
   <div id="card-2" class="card hidden bg-blue-100 p-6 rounded-lg shadow-md text-center
        flex items-center justify-center flex-col bg-cover bg-no-repeat bg-center relative" 
-       style="background-image: url('/images/cartee2.png'); width: 1000px; max-width: 100%; height: 900px;">
+       style="background-image: url('/images/cartee2.png'); width: 700px; max-width: 100%; height: 500px;">
+       <div class="flex ">
+<img 
+  src="/images/iconannif.png" 
+  alt="Icône anniversaire" 
+  class="w-25 h-20 mr-5 mb-10 "
+/>
+<img 
+  src="/images/iconannif.png" 
+  alt="Icône anniversaire" 
+  class="w-25 h-20 mr-5 "
+/>
+<img 
+  src="/images/iconannif.png" 
+  alt="Icône anniversaire" 
+  class="w-25 h-20 mr-5"
+/>
+
+</div>
     <p class="text-black font-bold text-lg">Joyeux anniversaire!</p>
     <p class="text-black font-semibold text-xl">Félicitations pour cette nouvelle année de vie.</p>
     <p class="text-black font-normal text-base">Que tous tes rêves deviennent réalité !</p>
-    <button class="absolute bottom-4 right-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition">Envoyer</button>
+    <button  style="background-color: #e0115f;" class="absolute bottom-4 right-4  text-white py-2 px-4 rounded hover:bg-blue-600 transition">Envoyer</button>
   </div>
 
   <!-- Card 3 -->
   <div id="card-3" class="card hidden bg-blue-100 p-6 rounded-lg shadow-md text-center
        flex items-center justify-center flex-col bg-cover bg-no-repeat bg-center relative" 
-       style="background-image: url('/images/carte3.png'); width: 1000px; max-width: 100%; height: 900px;">
-    <p class="text-black font-semibold text-3xl">Que ton anniversaire soit rempli de rires et de moments précieux.</p>
-    <p class="text-black font-semibold text-2xl">Heureux jour</p>
-    <p>Passe une excellente journée !</p>
-    <button class="absolute bottom-4 right-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition">Envoyer</button>
+       style="background-image: url('/images/carte3.png'); width: 700px; max-width: 100%; height: 500px;">
+       <div class="flex ">
+        <img 
+        src="/images/feux.png" 
+        alt="Icône anniversaire" 
+        class="w-25 h-20 mr-5"
+      />
+        <img 
+        src="/images/feux.png" 
+        alt="Icône anniversaire" 
+        class="w-25 h-20 mt-10  mr-5"
+      />
+        <img 
+        src="/images/feux.png" 
+        alt="Icône anniversaire" 
+        class="w-25 h-20 mr-5"
+      />
+       </div>
+       <div class="shadow-lg p-6 rounded-full mt-20 bg-white">
+    <p class="text-black font-semibold text-xl">Que ton anniversaire </p>
+    <p class="text-black font-semibold text-xl">soit rempli de rires</p>
+    <p class="text-black font-semibold text-xl">et de moments précieux.</p>
+        <p class="text-black font-semibold text-xl">Heureux jour!!</p>
+  </div>
+    <button  style="background-color: #e0115f;"class="absolute bottom-4 right-4  text-white py-2 px-4 rounded hover:bg-blue-600 transition">Envoyer</button>
   </div>
 
   <!-- Card 4 -->
   <div id="card-4" class="card hidden bg-blue-100 p-6 rounded-lg shadow-md text-center
        flex items-center justify-center flex-col bg-cover bg-no-repeat bg-center relative" 
-       style="background-image: url('/images/carte4.png');width: 1000px; max-width: 100%; height: 900px;">
-    <p class="text-black font-semibold text-3xl">Bon anniversaire !</p>
-    <p class="text-black font-semibold text-2xl">Que la joie et la réussite t'accompagnent tout au long de l'année à venir.</p>
-    <button class="absolute bottom-4 right-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition">Envoyer</button>
+       style="background-image: url('/images/carte4.png');width: 700px; max-width: 100%; height: 500px;">
+       <div class=" p-6  mt-40">
+    <p class="text-black font-semibold text-xl">Bon anniversaire !</p>
+    <p class="text-black font-semibold text-xl">Que la joie et la réussite</p>
+    <p class="text-black font-semibold text-xl">t'accompagnent tout au long</p>
+    <p class="text-black font-semibold text-xl"> de l'année à venir.</p>
+       </div>
+    <button  style="background-color: #e0115f;" class="absolute bottom-4 right-4 text-white py-2 px-4 rounded hover:bg-blue-600 transition">Envoyer</button>
   </div>
 
   <!-- Card 5 -->
   <div id="card-5" class="card hidden bg-blue-100 p-6 rounded-lg shadow-md text-center
        flex items-center justify-center flex-col bg-cover bg-no-repeat bg-center relative" 
-       style="background-image: url('/images/carte5.png');width: 1000px; max-width: 100%; height: 900px;">
-    <p class="text-black font-semibold text-2xl">Tous mes vœux de bonheur pour cette journée spéciale.</p>
-    <p class="text-black font-semibold text-2xl">Que l'amour et la chance soient avec toi !</p>
-    <button class="absolute bottom-4 right-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition">Envoyer</button>
+       style="background-image: url('/images/carte5.png');width: 700px; max-width: 100%; height: 500px;">
+       <div class=" p-6  mb-15">
+    <p class="text-black font-semibold text-2xl">Tous mes vœux de bonheur </p>
+    <p class="text-black font-semibold text-2xl">pour cette journée spéciale.</p>
+    <p class="text-black font-semibold text-2xl">Que l'amour et la chance</p>
+    <p class="text-black font-semibold text-2xl">soient avec toi !</p>
+  </div>
+    <button style="background-color: #e0115f;" class="absolute bottom-4 right-4  text-white py-2 px-4 rounded hover:bg-blue-600 transition">Envoyer</button>
   </div>
 
   <!-- Card 6 -->
-  <div id="card-6" class="card hidden bg-blue-100 p-6 rounded-full shadow-md text-center
+  <div id="card-6" class="card hidden bg-blue-100 p-6 rounded-lg shadow-md text-center
   flex items-center justify-center flex-col bg-cover bg-no-repeat bg-center relative" 
-  style="background-image: url('/images/design.png'); width: 1000px; max-width: 100%; height: 900px;">
-  <div class="shadow-lg p-6 rounded-lg bg-white">
-    <p class="text-black font-semibold text-3xl">Joyeux anniversaire!</p>
-    <p class="text-black font-semibold text-3xl">Que cette nouvelle année soit encore meilleure que la précédente.</p>
+  style="background-image: url('/images/design.png'); width: 700px; max-width: 100%; height: 500px;">
+  <div class="bg-white w-60 h-60 rounded-full   items-center justify-center text-black">
+    <p class="text-black mt-14 font-semibold text-xl">Joyeux anniversaire!</p>
+    <p class="text-black font-semibold text-xl">Que cette nouvelle</p>
+    <p class="text-black font-semibold text-xl">année soit encore </p>
+    <p class="text-black font-semibold text-xl"> meilleure que la précédente.</p>
   </div>
   
-<button class="absolute bottom-4 right-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition">Envoyer</button>
+<button  style="background-color: #e0115f;" class="absolute bottom-4 right-4  text-white py-2 px-4 rounded hover:bg-blue-600 transition">Envoyer</button>
 </div>
 
 </section>
