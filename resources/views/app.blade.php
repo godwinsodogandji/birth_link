6ee665f79bf7e4863d34006c26f590edb879713b
Christiane Sossoukpe <christdebo68@gmail.com>
2024-11-18 10:01:52 +0100

    correction de la duplication de vite etant à la base de l'erreur de pluging et installation des fonts awesome.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,7 +10,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Charme&display=swap" rel="stylesheet">
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="{{ asset('fonts/fonts.css') }}">
+        {{-- <link rel="stylesheet" href="{{ asset('fonts/fonts.css') }}"> --}}
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
         <!-- Scripts -->
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [
-        vue(),
         laravel({
             input: ['resources/js/app.js', 'resources/css/app.css'],
             refresh: true,
         }),
-        vue(), // Assurez-vous que le plugin Vue est ajouté ici
+        vue(), 
     ],
 });
