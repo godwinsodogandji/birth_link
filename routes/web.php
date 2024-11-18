09db52f3bcf1af26b7abf43d9978664ac2a25433
Akissohje esther <estherakissohe176@gmail.com>
2024-11-18 10:42:29 +0100

    oki essaie

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AmisSuggererController;
 use App\Http\Controllers\AnniversaireAvenirController;
 use App\Http\Controllers\AnniversairePasseController;
 use App\Http\Controllers\CardBitthdayController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DashbordController;
 use App\Http\Controllers\FriendController;
 use App\Http\Controllers\NotificationController;
@@ -14,8 +15,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/dashboard', [DashbordController::class, 'index'])->name('dashbord');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 Route::get('/suggestion-des-amis', [AmisSuggererController::class, 'index'])->name('suggestion-des-amis');
 
 Route::get('/notification', [NotificationController::class, 'index'])->name('notification');
@@ -27,7 +27,7 @@ Route::get('/confirmation-des-amis', [AmisSuggererController::class, 'confirmati
 Route::get('/friends', [FriendController::class, 'index'])->name('friends');
 
 Route::get('/ajoutdesamis', [AjoutDesAmisController::class, 'index'])->name('ajoutdesamis');
-Route::get('/dashboard', [DashbordController::class, 'index'])->name('dashbord');
+// Route::get('/dashboard', [DashbordController::class, 'index'])->name('dashbord');
 Route::get('/anniversaire-avenir', [AnniversaireAvenirController::class, 'index'])->name('anniversaire-avenir');
 Route::get('/anniversaire-passe', [AnniversairePasseController::class, 'index'])->name('anniversaire-passe');
 Route::get('/theme', [ThemepersonController::class, 'index'])->name('theme');
